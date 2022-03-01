df973d6fbdd93bd2d6e42bc486e4b97878238e95
wjtje <33957974+wjtje@users.noreply.github.com>
2022-03-01 21:11:06 +0100

    📱 Updated navigation bar with mobile support

--- src/lib/components/Navigation/Navigation.svelte
+++ src/lib/components/Navigation/Navigation.svelte
@@ -8,9 +8,11 @@
 		@apply sticky top-0 left-0 w-full h-12 z-10;
 		// Position the children at the center of the navigation bar
 		@apply flex items-center gap-4 px-6;
-
+		// Border and color
 		@apply border-b-2;
 		@apply bg-green-600 border-green-500 text-white;
 		@apply gdark:bg-green-700 gdark:border-green-600;
+		// Spacing of the items
+		@apply justify-between md:justify-start;
 	}
 </style>
--- src/lib/components/Navigation/NavigationItems.svelte
+++ src/lib/components/Navigation/NavigationItems.svelte
@@ -1,10 +1,53 @@
-<section>
-	<slot />
-</section>
+<script lang="ts">
+	import { fade } from 'svelte/transition';
+
+	let showMobileMenu = false;
+
+	const handleClick = () => {
+		showMobileMenu = !showMobileMenu;
+	};
+</script>
+
+<button on:click={handleClick}> = </button>
+
+{#if showMobileMenu}
+	<section transition:fade={{ duration: 150 }} class="mobile">
+		<button on:click={handleClick}> x </button>
+		<slot />
+	</section>
+{:else}
+	<section>
+		<slot />
+	</section>
+{/if}
 
 <style lang="scss">
+	button {
+		@apply md:hidden;
+	}
+
 	section {
-		@apply flex-grow;
-		@apply flex gap-4;
+		// On larger screen make it full width
+		@apply md:flex-grow;
+		// On small screen stack the items,
+		// and on larger screens display the side-by-side
+		@apply hidden md:flex gap-1 md:gap-4 flex-col md:flex-row;
+		// On small screen make it full screen,
+		// and on larger screens make it inline
+		@apply absolute top-0 left-0 w-full h-screen bg-gray-700;
+		@apply md:relative md:w-auto md:h-auto md:bg-transparent;
+
+		button {
+			// Display the close button at the top left
+			@apply absolute top-0 right-0;
+		}
+
+		& > :global(*) {
+			@apply px-2;
+		}
+
+		&.mobile {
+			@apply flex;
+		}
 	}
 </style>
