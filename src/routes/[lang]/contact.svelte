387baa705ee868df3f86301ac034cd4953748c23
wjtje <33957974+wjtje@users.noreply.github.com>
2022-03-03 21:15:27 +0100

    💄 Added custom hover effect to contact list

--- src/routes/[lang]/contact.svelte
+++ src/routes/[lang]/contact.svelte
@@ -6,6 +6,7 @@
 	import GoMarkGithub from 'svelte-icons/go/GoMarkGithub.svelte';
 	import FaLinkedin from 'svelte-icons/fa/FaLinkedin.svelte';
 	import FaTwitter from 'svelte-icons/fa/FaTwitter.svelte';
+	import FaReddit from 'svelte-icons/fa/FaReddit.svelte';
 </script>
 
 <svelte:head>
@@ -16,15 +17,20 @@
 <h1>{capitalizeFirstLetter($t('contact.title'))}</h1>
 
 <ul>
-	<li aria-label="email"><MdEmail /><span><a href="mailto:me@wjtje.ga">me@wjtje.ga</a></span></li>
-	<li aria-label="Github">
+	<li aria-label="email" class="email">
+		<MdEmail /><span><a href="mailto:me@wjtje.ga">me@wjtje.ga</a></span>
+	</li>
+	<li aria-label="Github" class="github">
 		<GoMarkGithub /><span><a href="https://www.github.com/wjtje">wjtje</a></span>
 	</li>
-	<li aria-label="LinkedIn">
+	<li aria-label="LinkedIn" class="linkedin">
 		<FaLinkedin /><span><a href="https://www.linkedin.com/in/wjtje/">Wouter van der Wal</a></span>
 	</li>
-	<li aria-label="Twitter">
-		<FaTwitter /><span><a href="https://twitter.com/wjtje_wal">wjtje_wal</a></span>
+	<li aria-label="Reddit" class="reddit">
+		<FaReddit /><span><a href="https://www.reddit.com/u/wjtje">u/wjtje</a></span>
+	</li>
+	<li aria-label="Twitter" class="twitter">
+		<FaTwitter /><span><a href="https://twitter.com/wjtje_wal">@wjtje_wal</a></span>
 	</li>
 </ul>
 
@@ -35,17 +41,27 @@
 
 	ul {
 		li {
-			@apply h-8 flex flex-row gap-2 items-center mb-2;
+			@apply h-8 flex flex-row gap-2 items-center mb-2 transition-[color] duration-200 w-auto;
 
 			:global(svg) {
-				@apply h-8 w-8 p-1;
+				@apply h-8 w-8 p-1 fill-current;
 			}
 
 			span {
-				@apply flex-grow h-auto;
+				@apply h-auto whitespace-nowrap;
+			}
 
-				a {
-					@apply hover:text-sky-400 transition-[color];
+			// Custom hover effects
+			&.email {
+				@apply hover:text-red-600 dark:hover:text-red-500;
+			}
+
+			// Define brands with custom styles
+			$brands: linkedin, reddit, twitter;
+
+			@each $brand in $brands {
+				&.#{$brand} {
+					@apply hover:text-#{$brand}-brand;
 				}
 			}
 		}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,13 @@ module.exports = {
 	content: ['./src/**/*.{html,svelte,ts,js}'],
 	darkMode: 'class',
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				'reddit-brand': '#ff4500',
+				'twitter-brand': '#009EF7',
+				'linkedin-brand': '#0A66C2'
+			}
+		}
 	},
 	plugins: [require('tailwindcss-global-dark')]
-};
+}
