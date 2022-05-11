ee19f6883c9531e4fdc4c603dc012f3718104ed3
wjtje <33957974+wjtje@users.noreply.github.com>
2022-05-11 08:46:35 +0200

    💄 Change loading color based on theme

--- src/lib/components/ActivityLoader.svelte
+++ src/lib/components/ActivityLoader.svelte
@@ -1,10 +1,32 @@
 <script lang="ts">
+	import { browser } from '$app/env';
+	import { writableLocal } from '$lib/store-localstorage';
 	import ContentLoader from 'svelte-content-loader';
 
-	// TODO: Dynamically change the color
+	let primaryColor = '#FFFFFF';
+	let secondaryColor = '#e5e7eb';
+
+	// Store the value inside localStorage
+	const store = writableLocal('theme', 'system');
+
+	$: {
+		if (browser && document) {
+			let theme = $store;
+
+			if (
+				(theme == 'system' &&
+					window.matchMedia &&
+					window.matchMedia('(prefers-color-scheme: dark)').matches) ||
+				theme == 'dark'
+			) {
+				primaryColor = '#111827';
+				secondaryColor = '#1f2937';
+			}
+		}
+	}
 </script>
 
-<ContentLoader primaryColor="#111827" secondaryColor="#1f2937" speed="1.5" width="252" height="212">
+<ContentLoader {primaryColor} {secondaryColor} speed="1.5" width="252" height="212">
 	<rect width="64" height="12" rx="2" />
 	<rect y="16" width="252" height="20" rx="2" />
 	<rect y="40" width="166" height="16" rx="2" />
