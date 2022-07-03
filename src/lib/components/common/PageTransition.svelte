608c87b53837a3941b86d7aec3daea2620dc1821
wjtje <33957974+wjtje@users.noreply.github.com>
2022-07-03 21:10:20 +0200

    ✨ Added loading animation

--- src/lib/components/common/PageTransition.svelte
+++ src/lib/components/common/PageTransition.svelte
@@ -1,32 +1,70 @@
 <script lang="ts">
+	import { afterNavigate, beforeNavigate } from '$app/navigation';
 	import { fly } from 'svelte/transition';
 
 	export let url: URL;
-	let cleanUrl = '';
+
+	/// This contains the current page is shown right now
+	let currentPage = '';
+	/// This indicated if a new page is loading
+	let isLoading = false;
 
 	$: {
 		// This makes sure the page transition is only triggered when the page actually changes
 		const parts = url.pathname.split('/');
-		cleanUrl = parts.slice(2).join('/');
+		currentPage = parts.slice(2).join('/');
 	}
+
+	// Update the loading state
+	beforeNavigate(() => {
+		isLoading = true;
+	});
+
+	afterNavigate(() => {
+		isLoading = false;
+	});
 
 	let duration = 250;
 </script>
 
-<div>
-	{#key cleanUrl}
+<div class="content">
+	{#key currentPage}
 		<main in:fly={{ x: -5, duration, delay: duration }} out:fly={{ x: 5, duration }}>
 			<slot />
 		</main>
 	{/key}
+
+	{#if isLoading}
+		<div class="loader" />
+	{/if}
 </div>
 
 <style lang="scss">
-	div {
+	div.content {
 		@apply overflow-y-scroll overflow-x-hidden h-[calc(100vh-3rem)];
+	}
+
+	div.loader {
+		@apply absolute top-12 w-screen h-1 z-50 bg-green-500;
+		animation: load-right 2s 1s cubic-bezier(0.645, 0.045, 0.355, 1) forwards;
+		animation-iteration-count: infinite;
+		left: -100vw;
+		transform-origin: left;
 	}
 
 	main {
 		@apply container mx-auto py-4 lg:py-8 px-4 lg:px-8 pb-16;
+	}
+
+	@keyframes load-right {
+		0% {
+			left: -100vw;
+		}
+		50% {
+			left: 0vw;
+		}
+		100% {
+			left: 100vw;
+		}
 	}
 </style>
