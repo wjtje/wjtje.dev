9f133173388ea5a80158f47c0af30d236c7c7b52
wjtje <33957974+wjtje@users.noreply.github.com>
2022-07-05 11:43:21 +0200

    ✨ Added MemberEvent and fixed a couple of small mistakes

--- src/lib/components/home/GithubActivity/ActivityItem.svelte
+++ src/lib/components/home/GithubActivity/ActivityItem.svelte
@@ -10,7 +10,7 @@
 	{#if event.type == 'CommitCommentEvent'}
 		<MiniPost
 			date={event.created_at}
-			title={$t(`GitHubActivity.${event.type}`, {
+			title={$t(`GitHubActivity.CommitCommentEvent`, {
 				...event.payload?.comment,
 				repo: event.repo.name
 			})}
@@ -22,13 +22,16 @@
 			title={$t(`GitHubActivity.${event.type}.${event.payload?.ref_type ?? 'unknown'}`, {
 				...event.payload,
 				repo: event.repo.name,
-				id: event.id
+				id: event.id,
+				default: $t(`GitHubActivity.${event.type}.unknown`, {
+					id: event.id
+				})
 			})}
 		/>
 	{:else if event.type == 'ForkEvent'}
 		<MiniPost
 			date={event.created_at}
-			title={$t(`GitHubActivity.${event.type}`, {
+			title={$t(`GitHubActivity.ForkEvent`, {
 				...event.payload?.forkee,
 				repo: event.repo.name
 			})}
@@ -36,31 +39,33 @@
 	{:else if event.type == 'GollumEvent'}
 		<MiniPost
 			date={event.created_at}
-			title={$t(`GitHubActivity.${event.type}`, {
+			title={$t(`GitHubActivity.GollumEvent`, {
 				...event.payload,
 				repo: event.repo.name
 			})}
-			subtitle={$t(`GitHubActivity.${event.type}Subtitle`, {
+			subtitle={$t(`GitHubActivity.GollumEventSubtitle`, {
 				pages: event.payload?.pages?.length ?? 0
 			})}
 		/>
 	{:else if event.type == 'IssueCommentEvent'}
-		<MiniPost
-			date={event.created_at}
-			title={$t(`GitHubActivity.${event.type}.${event.payload?.action ?? 'unknown'}`, {
-				issue_html: event.payload?.issue?.html_url,
-				repo: event.repo.name,
-				id: event.id
-			})}
-			subtitle={event.payload?.action == 'created' || event.payload?.action == 'edited'
-				? event.payload?.comment?.body
-				: event.payload?.issue?.title}
-		/>
+		{#if event.payload?.action == 'opened' || event.payload?.action == 'edited' || event.payload?.action == 'closed'}
+			<MiniPost
+				date={event.created_at}
+				title={$t(`GitHubActivity.IssueCommentEvent.${event.payload?.action}`, {
+					issue_html: event.payload?.issue?.html_url,
+					repo: event.repo.name,
+					id: event.id
+				})}
+				subtitle={event.payload?.action == 'created' || event.payload?.action == 'edited'
+					? event.payload?.comment?.body
+					: event.payload?.issue?.title}
+			/>
+		{/if}
 	{:else if event.type == 'IssuesEvent'}
 		{#if event.payload?.action == 'opened' || event.payload?.action == 'edited' || event.payload?.action == 'closed'}
 			<MiniPost
 				date={event.created_at}
-				title={$t(`GitHubActivity.${event.type}.${event.payload?.action ?? 'unknown'}`, {
+				title={$t(`GitHubActivity.IssuesEvent.${event.payload?.action}`, {
 					issue_html: event.payload?.issue?.html_url,
 					repo: event.repo.name,
 					id: event.id
@@ -68,10 +73,21 @@
 				subtitle={event.payload?.issue?.title}
 			/>
 		{/if}
+	{:else if event.type == 'MemberEvent'}
+		{#if event.payload?.action == 'added'}
+			<MiniPost
+				date={event.created_at}
+				title={$t('GitHubActivity.MemberEvent.added', {
+					member: event.payload?.member?.login,
+					member_url: event.payload?.member?.url,
+					repo: event.repo.name
+				})}
+			/>
+		{/if}
 	{:else if event.type == 'PublicEvent'}
 		<MiniPost
 			date={event.created_at}
-			title={$t(`GitHubActivity.${event.type}`, {
+			title={$t(`GitHubActivity.PublicEvent`, {
 				repo: event.repo.name
 			})}
 		/>
@@ -79,11 +95,11 @@
 		{#if event.payload?.action == 'opened' || event.payload?.action == 'edited' || event.payload?.action == 'closed' || event.payload?.action == 'reopened'}
 			<MiniPost
 				date={event.created_at}
-				title={$t(`GitHubActivity.${event.type}.${event.payload?.action ?? 'unknown'}`, {
+				title={$t(`GitHubActivity.PullRequestEvent.${event.payload?.action}`, {
 					repo: event.repo.name,
 					id: event.id
 				})}
-				subtitle={$t(`GitHubActivity.${event.type}.subtitle`, {
+				subtitle={$t(`GitHubActivity.PullRequestEvent.subtitle`, {
 					...event.payload?.pull_request
 				})}
 			/>
@@ -91,7 +107,7 @@
 	{:else if event.type == 'PullRequestReviewEvent'}
 		<MiniPost
 			date={event.created_at}
-			title={$t(`GitHubActivity.${event.type}`, {
+			title={$t(`GitHubActivity.PullRequestReviewEvent`, {
 				review_url: event.payload?.review?.html_url,
 				pull_request_url: event.payload?.pull_request?.html_url,
 				pull_request: event.payload?.pull_request?.title,
@@ -101,7 +117,7 @@
 	{:else if event.type == 'PullRequestReviewCommentEvent'}
 		<MiniPost
 			date={event.created_at}
-			title={$t(`GitHubActivity.${event.type}`, {
+			title={$t(`GitHubActivity.PullRequestReviewCommentEvent`, {
 				comment_url: event.payload?.comment?.html_url,
 				pull_request_url: event.payload?.pull_request?.html_url,
 				pull_request: event.payload?.pull_request?.title,
@@ -111,16 +127,16 @@
 	{:else if event.type == 'PushEvent'}
 		<MiniPost
 			date={event.created_at}
-			title={$t(`GitHubActivity.${event.type}`, {
+			title={$t(`GitHubActivity.PushEvent`, {
 				size: event.payload?.size,
 				repo: event.repo.name
 			})}
-			subtitle={$t(`GitHubActivity.${event.type}Subtitle`, {
+			subtitle={$t(`GitHubActivity.PushEventSubtitle`, {
 				commit: event.payload?.commits[0]?.message ?? $t('GitHubActivity.PushEventSubtitleEmpty')
 			}) +
 				(event.payload?.commits.length > 1
 					? ' ' +
-					  $t(`GitHubActivity.${event.type}SubtitleMore`, {
+					  $t(`GitHubActivity.PushEventSubtitleMore`, {
 							count: event.payload?.commits.length - 1
 					  })
 					: '')}
@@ -128,7 +144,7 @@
 	{:else if event.type == 'ReleaseEvent'}
 		<MiniPost
 			date={event.created_at}
-			title={$t(`GitHubActivity.${event.type}`, {
+			title={$t(`GitHubActivity.ReleaseEvent`, {
 				repo: event.repo.name,
 				html_url: event.payload?.release.html_url,
 				name: event.payload?.release.name
@@ -138,7 +154,7 @@
 	{:else if event.type == 'WatchEvent'}
 		<MiniPost
 			date={event.created_at}
-			title={$t(`GitHubActivity.${event.type}`, {
+			title={$t(`GitHubActivity.WatchEvent`, {
 				repo: event.repo.name
 			})}
 		/>
