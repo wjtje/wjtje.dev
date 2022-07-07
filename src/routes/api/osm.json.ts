dd7d510e83a47d16e1c4d7e93a7c678285363797
wjtje <33957974+wjtje@users.noreply.github.com>
2022-07-07 19:17:14 +0200

    🚧 StreetComplete icons

--- src/routes/api/osm.json.ts
+++ src/routes/api/osm.json.ts
@@ -8,7 +8,10 @@ import {
 import { prisma } from '$lib/prisma';
 import type { RequestHandler } from './__types/osm.json';
 import { fetchOsmData } from '$lib/api/osm';
-import { getStreetCompleteDetails } from '$lib/api/getStreetcompleteDetails';
+import {
+	getStreetCompleteImage,
+	updateStreetCompleteCache
+} from '$lib/api/getStreetcompleteDetails';
 
 export const get: RequestHandler = async () => {
 	// Get information about the cache
@@ -20,6 +23,9 @@ export const get: RequestHandler = async () => {
 		// Fetch the data
 		try {
 			const responseChangesets = await fetchOsmData();
+
+			// Update streetComplete cache
+			await updateStreetCompleteCache();
 
 			// Remove old cache
 			await prisma.remoteData.deleteMany({
@@ -74,7 +80,7 @@ export const get: RequestHandler = async () => {
 									version: changeset.parsedTags.created_by.version
 								}
 							};
-							data.image = getStreetCompleteDetails(
+							data.image = await getStreetCompleteImage(
 								changeset.parsedTags['StreetComplete:quest_type']
 							);
 							break;
