5ab161c58067defbaef61a43f737cece33be122b
wjtje <33957974+wjtje@users.noreply.github.com>
2022-08-01 09:40:02 +0200

    🚧 Basic custom error page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,23 @@
+<script lang="ts" context="module">
+	import type { Load } from '@sveltejs/kit';
+
+	export const load: Load = ({ error, status }) => {
+		return {
+			props: {
+				status
+			}
+		};
+	};
+</script>
+
+<script lang="ts">
+	import E404 from '$lib/components/error/404.svelte';
+
+	export let status: number;
+</script>
+
+{#if status == 404}
+	<E404 />
+{:else}
+	<h1>Unknown error</h1>
+{/if}
