6e4321c05b7f364136a192cea42ace195dfb5b18
Wouter van der Wal <33957974+wjtje@users.noreply.github.com>
2022-08-05 16:40:38 +0200

    ✨ Custom error pages (#136)
    * 🚧 Basic custom error page
    
    * 🥚 HTCPCP
    
    * 🚧 Added 500 support
    
    * 🚧 Give error details in console

---
+++ src/lib/components/error/404.svelte
@@ -0,0 +1,3 @@
+<h1>Whoops, 404</h1>
+
+<p>The page you where looking for doesn't exsist</p>
---
+++ src/lib/components/error/500.svelte
@@ -0,0 +1,3 @@
+<h1>500 - Something went wrong</h1>
+
+<p>The server had an internal error and could not complete your request</p>
