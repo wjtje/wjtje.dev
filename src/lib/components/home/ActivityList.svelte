4fb156892dfbe3851656d3aed14279a5d0589420
wjtje <33957974+wjtje@users.noreply.github.com>
2022-08-06 16:09:59 +0200

    🌐 Added i18n support for the new way of translating

--- src/lib/components/home/ActivityList.svelte
+++ src/lib/components/home/ActivityList.svelte
@@ -1,39 +1,32 @@
 <script lang="ts">
 	import { locale, t } from '$lib/i18n';
-	import { onMount } from 'svelte';
 	import { scale } from 'svelte/transition';
 	import MiniPostLoader from '$lib/components/common/MiniPostLoader.svelte';
-	import type { RemoteData } from '@prisma/client';
 	import MiniPost from '../common/MiniPost.svelte';
 
 	export let activityName: string;
 
-	// TODO: Auto language update
-	onMount(async () => {
+	const getDataFromServer = async (lang: string) => {
 		const response = await fetch(`/api/${activityName}.json?lang=${locale.get()}`);
-		const json = await response.json();
+		return await response.json();
+	};
 
-		events = json;
-		status = response.status;
-	});
-
-	let events: RemoteData[] = [];
-	let status: number;
+	$: response = getDataFromServer($locale);
 </script>
 
-{#if status == undefined}
+{#await response}
 	<MiniPostLoader />
-{:else if status != 200}
-	<h3>{$t('home.dataLoadingFailed', { status: status })}</h3>
-{/if}
-
-{#each events as event, i}
-	<div in:scale={{ duration: 400, delay: i * 50 }}>
-		<MiniPost
-			title={event.mainTitle}
-			subtitle={event.subTitle}
-			date={event.date}
-			image={event.image}
-		/>
-	</div>
-{/each}
+{:then events}
+	{#each events as event, i}
+		<div in:scale={{ duration: 400, delay: i * 50 }}>
+			<MiniPost
+				title={event.mainTitle}
+				subtitle={event.subTitle}
+				date={event.date}
+				image={event.image}
+			/>
+		</div>
+	{/each}
+{:catch error}
+	<h3>{$t('home.dataLoadingFailed', { status: error.message })}</h3>
+{/await}
