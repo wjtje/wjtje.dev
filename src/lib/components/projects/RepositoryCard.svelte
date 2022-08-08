861734ba11031affd58057ed37476cc60eaa67a2
Robin van der Linde <robin.vanderlinde@gmail.com>
2022-08-08 22:00:59 +0200

    ✨ Add stars and fork count

--- src/lib/components/projects/RepositoryCard.svelte
+++ src/lib/components/projects/RepositoryCard.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import LanguageIndicator from './LanguageIndicator.svelte';
+	import GoRepoForked from 'svelte-icons/go/GoRepoForked.svelte';
+	import GoStar from 'svelte-icons/go/GoStar.svelte';
 
 	export let name: string;
 	export let description: string;
@@ -8,6 +10,8 @@
 	export let parentName: string;
 	export let parentUrl: string;
 	export let language: string;
+	export let forksCount: number;
+	export let stargazersCount: number;
 	export let topics: string;
 	export let image: string;
 
@@ -24,8 +28,14 @@
 	{#if language}
 		<LanguageIndicator {language} />
 	{/if}
+	{#if forksCount > 0}
+		<span><GoRepoForked />{forksCount}</span>
+	{/if}
+	{#if stargazersCount > 0}
+		<span><GoStar />{stargazersCount}</span>
+	{/if}
 	{#if fork}
-		<span class="fork">Forked from <a href={parentUrl} class="fork">{parentName}</a></span>
+		<span class="fork">Forked from <a href={parentUrl}>{parentName}</a></span>
 	{/if}
 	{#if description}
 		<p>{description}</p>
@@ -40,23 +50,28 @@
 <style lang="scss">
 	section {
 		@apply border-[1px] border-gray-800 rounded-lg mb-1 p-1;
-	}
-	img {
-		@apply float-right h-20;
-	}
-	a {
-		@apply hover:underline;
-	}
-	a.name {
-		@apply text-xl font-bold text-blue-700 hover:underline hover:text-blue-500;
-	}
-	a.fork {
-		@apply text-gray-600 hover:text-gray-500;
-	}
-	span {
-		@apply text-xs text-gray-700 mb-[-0.1rem] block;
-	}
-	p.topics {
-		@apply text-gray-600;
+
+		img {
+			@apply float-right h-20;
+		}
+		a {
+			@apply hover:underline;
+		}
+		a.name {
+			@apply text-xl font-bold text-blue-700 hover:text-blue-500;
+		}
+		span.fork {
+			@apply text-xs text-gray-700 mb-[-0.1rem] block;
+			a {
+				@apply text-gray-600 hover:text-gray-500;
+			}
+		}
+		p.topics {
+			@apply text-gray-600;
+		}
+
+		:global(svg) {
+			@apply h-5 w-5 inline fill-gray-700;
+		}
 	}
 </style>
