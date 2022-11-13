af071d7b0135b945b9864dfa3c0aa0faac3f299d
wjtje <33957974+wjtje@users.noreply.github.com>
2022-11-13 22:10:27 +0100

    💄 Changed particle loading animation

--- src/lib/components/common/Particles.svelte
+++ src/lib/components/common/Particles.svelte
@@ -2,6 +2,7 @@
 	import { browser } from '$app/environment';
 
 	import { onMount } from 'svelte';
+	import { fade } from 'svelte/transition';
 
 	import Particles from 'svelte-particles';
 	import { loadFull } from 'tsparticles';
@@ -133,11 +134,13 @@
 	});
 </script>
 
-<div use:styles={{ height: `${height}px` }}>
-	<Particles
-		id="tsparticles"
-		options={particlesConfig}
-		on:particlesLoaded={onParticlesLoaded}
-		{particlesInit}
-	/>
-</div>
+{#key height}
+	<div use:styles={{ height: `${height}px` }} in:fade={{ delay: 500 }}>
+		<Particles
+			id="tsparticles"
+			options={particlesConfig}
+			on:particlesLoaded={onParticlesLoaded}
+			{particlesInit}
+		/>
+	</div>
+{/key}
