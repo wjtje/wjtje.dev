40d5d77d036d06148c497ec76913c2b5a5f936ec
Robin van der Linde <robin.vanderlinde@gmail.com>
2022-11-21 00:31:06 +0100

    ✨ Use OSM JSON API instead of XML API

--- src/lib/@types/osm.ts
+++ src/lib/@types/osm.ts
@@ -2,12 +2,6 @@ export interface Xml {
 	'@_version': string;
 	'@_encoding': string;
 }
-
-export interface Tag {
-	'@_k': string;
-	'@_v': string;
-}
-
 export interface OsmEditor {
 	/**
 	 * The name of the editor
@@ -44,32 +38,118 @@ export interface ParsedTags {
 }
 
 export interface Changeset {
-	tag: Tag[];
-	parsedTags: ParsedTags;
-	'@_id': string;
-	'@_created_at': Date;
-	'@_open': string;
-	'@_comments_count': string;
-	'@_changes_count': string;
-	'@_closed_at': Date;
-	'@_min_lat': string;
-	'@_min_lon': string;
-	'@_max_lat': string;
-	'@_max_lon': string;
-	'@_uid': string;
-	'@_user': string;
+	/**
+	 * ID of the changeset
+	 * URL: https://www.openstreetmap.org/changeset/{id}
+	 *
+	 * @example 129131578
+	 */
+	id: number;
+
+	/**
+	 * Date when the changeset was created
+	 */
+	created_at: Date;
+
+	/**
+	 * Whether this changeset is still open
+	 */
+	open: boolean;
+
+	/**
+	 * How many comments this changeset has
+	 */
+	comments_count: number;
+
+	/**
+	 * How many changes this changeset contains
+	 */
+	changes_count: number;
+
+	/**
+	 * When the changeset was closed
+	 */
+	closed_at: Date;
+
+	/**
+	 * Minimum latitude of the changeset
+	 */
+	min_lat: number;
+
+	/**
+	 * Minimum longitude of the changeset
+	 */
+	min_lon: number;
+
+	/**
+	 * Maximum latitude of the changeset
+	 */
+	max_lat: number;
+
+	/**
+	 * Maximum longitude of the changeset
+	 */
+	max_lon: number;
+
+	/**
+	 * UID of the user who created this changeset
+	 */
+	uid: number;
+
+	/**
+	 * Username of the user who created this changeset
+	 */
+	user: string;
+
+	/**
+	 * Object containing the tags of the changeset
+	 */
+	tags: { [key: string]: string };
+
+	/**
+	 * Object containing the parsed tags of the changeset
+	 */
+	parsedTags?: ParsedTags;
 }
 
 export interface Osm {
-	changeset: Changeset[];
-	'@_version': string;
-	'@_generator': string;
-	'@_copyright': string;
-	'@_attribution': string;
-	'@_license': string;
-}
+	/**
+	 * Version of the API
+	 *
+	 * @example "0.6"
+	 */
+	version: string;
 
-export interface OsmRootObject {
-	'?xml': Xml;
-	osm: Osm;
+	/**
+	 * Generator of the response
+	 *
+	 * @example "OpenStreetMap server"
+	 */
+	generator: string;
+
+	/**
+	 * Copyright of the response
+	 *
+	 * @example "OpenStreetMap and contributors"
+	 */
+	copyright: string;
+
+	/**
+	 * Attribution URL of the response
+	 *
+	 * @example "http://www.openstreetmap.org/copyright"
+	 */
+	attribution: string;
+
+	/**
+	 * URL for the license of the response
+	 *
+	 * @example "http://opendatacommons.org/licenses/odbl/1-0/"
+	 */
+	license: string;
+
+	/**
+	 * Object containing all changesets
+	 */
+	changesets: Changeset[];
 }
